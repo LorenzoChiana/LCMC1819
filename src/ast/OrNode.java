a37938e4cf7ceddba630836fdd857be8c1ecef45
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2019-03-05 22:42:25 +0100

    New nodes

---
+++ src/ast/OrNode.java
@@ -0,0 +1,53 @@
+package ast;
+
+import lib.FOOLlib;
+
+public class OrNode implements Node {
+	private Node left;
+	private Node right;
+
+	public OrNode (Node l, Node r) {
+		left=l;
+		right=r;
+	}
+
+	public String toPrint(String s) {
+		return s+"And\n" + left.toPrint(s+"  ")   
+		+ right.toPrint(s+"  ") ; 
+	}
+
+	public Node typeCheck() {
+		Node l= left.typeCheck();  
+		Node r= right.typeCheck();  
+		if ( !(FOOLlib.isSubtype(l, r) || FOOLlib.isSubtype(r, l)) ) {
+			System.out.println("Incompatible types in and");
+			System.exit(0);	
+		}  
+		return new BoolTypeNode();
+	}
+
+	public String codeGeneration() {
+		String l1= FOOLlib.freshLabel();
+		String l2= FOOLlib.freshLabel();
+		String l3= FOOLlib.freshLabel();
+		return 
+				left.codeGeneration() + 
+				"push 1\n"+ 
+				"beq "+l1+"\n"+
+				"push 0\n"+
+				"b "+l2+"\n"+
+				l1+": \n"+
+				
+				right.codeGeneration() +
+				"push 1\n"+ 
+				"beq "+l3+"\n"+
+				"push 0\n"+
+				"b "+l2+"\n"+
+				l3+": \n"+
+				"push 1\n"+
+				
+				l2+": \n";
+	}
+
+}
+
