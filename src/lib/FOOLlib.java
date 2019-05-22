56577e029a5fa5460b89ed5697f46b40817144f7
Meshua Galassi <meshua.galassi@studio.unibo.it>
2019-05-22 14:56:05 +0200

    finito se giada è contenta

--- src/lib/FOOLlib.java
+++ src/lib/FOOLlib.java
@@ -20,14 +20,16 @@ public class FOOLlib {
 
 	//valuta se il tipo "a" e' <= al tipo "b"
 	public static boolean isSubtype (Node a, Node b) {
+		//se entrambi sono ArrowTypeNode controllo i parametri e il tipo di ritorno
 		if(a instanceof ArrowTypeNode && b instanceof ArrowTypeNode) {
 			ArrowTypeNode nodeA = (ArrowTypeNode)a;
 			ArrowTypeNode nodeB = (ArrowTypeNode)b;
-			if(nodeA.getParListLength() != nodeB.getParListLength() ||
-					!(isSubtype(nodeA.getRet(), nodeB.getRet()))) {
+			//covarianza per i tipi di ritorno: A deve essere sottotipo di B
+			if(nodeA.getParListLength() != nodeB.getParListLength() 
+					|| !(isSubtype(nodeA.getRet(), nodeB.getRet()))) {
 				return false;
 			} else {
-				//i tipi di ritorno devono essere sottotipi rispetto ai parametri
+				//controvarianza per i parametri: i parametri di B devono essere sottotipo dei parametri di A
 				int dimA = nodeA.getParListLength();
 				for(int i = 0; i < dimA; i++) {
 					if(!isSubtype(nodeB.getParList().get(i), nodeA.getParList().get(i))) {
@@ -37,42 +39,49 @@ public class FOOLlib {
 				return true;
 			}
 		} else 
-			/*Controllo che in superType esista una coppia con chiave a (sottotipo)
-			 * se esiste controllo che il super tipo di a sia uguale a b, se non lo e'
-			 * chiamo ricorsivamente (ora il sotto tipo sara'ï¿½ il super tipo di a e il super tipo rimane b
+			/*Controllo che in superType esista una coppia con chiave A (sottotipo)
+			 * se esiste controllo che il super tipo di A sia uguale a A, se non lo e'
+			 * chiamo ricorsivamente (ora il sotto tipo sara' il super tipo di A e il super tipo rimane B)
 			 * itero finche' non trovo una corrispondenza nella hashmap, se non la trovo vuol dire che
-			 * a non e' sottotipo di b
+			 * A non e' sottotipo di B
 			 * */
 			if (a instanceof RefTypeNode && b instanceof RefTypeNode) {
 				RefTypeNode refB = (RefTypeNode) b; // classe padre -> supertipo
 				RefTypeNode refA = (RefTypeNode) a; // classe figlio -> sottotipo
 				String superTypeA = superType.get(refA.getClassId());
-
+				
+				//se A e B sono la stessa classe torna true
 				if(refB.getClassId().equals(refA.getClassId())) {
 					return true;
 				}
-
+				
+				/* Se A non ha un supertipo il typecheck non va a buon fine 
+				 * poiche' A non e' sottoclasse di B */
 				if(superTypeA == null){
 					return false;
 				}
 
+				/* Chiamo ricorsivamente finchè non trovo una corrispondenza nella HashMap. 
+				 * Se trovo la corrispondenza il typecheck è corretto e A e' sottotipo di B */
 				if(!(refB.getClassId().equals(superTypeA))){
 					return isSubtype(new RefTypeNode(superTypeA), refB);
 				} else {
 					return true;
 				}
-			} else if(a instanceof EmptyTypeNode) { //EmptyTypeNode Ã¨ sottotipo di tutti i tipi
+			
+			//EmptyTypeNode e' sottotipo di tutti i tipi
+			} else if(a instanceof EmptyTypeNode) { 
 				return true;
-			} else if
-			(a.getClass().equals(b.getClass()) ||
-					((a instanceof BoolTypeNode) && (b instanceof IntTypeNode))){
+			} else if(a.getClass().equals(b.getClass()) 
+					|| ((a instanceof BoolTypeNode) && (b instanceof IntTypeNode))){
 				return true;
 			}
 		return false;
 	}
 
+	//il più basso antenato comune di due nodi
 	public static Node lowestCommonAncestor (Node a, Node b) {
-		//per a e b tipi bool/int
+		//per A e B tipi bool/int
 		if (a instanceof BoolTypeNode && b instanceof BoolTypeNode) {
 			return a;
 		} else if (a instanceof IntTypeNode && b instanceof IntTypeNode) {
@@ -84,16 +93,16 @@ public class FOOLlib {
 		} 
 
 		//per a e b tipi riferimento (o EmptyTypeNode)
-		// se uno tra "a" e "b" eÌ€ EmptyTypeNode torna l'altro;
+		// se uno tra A e B e' EmptyTypeNode torna l'altro;
 		if (a instanceof EmptyTypeNode && b instanceof RefTypeNode) {
 			return b;
 		} else if (b instanceof EmptyTypeNode && a instanceof RefTypeNode) {
 			return a;
 		}else if (a instanceof RefTypeNode && b instanceof RefTypeNode) {
-			/* all'inizio considera la classe di "a" e risale, poi, le sue superclassi controllando, ogni volta, se "b" 
+			/* all'inizio considera la classe di A e risale, poi, le sue superclassi controllando, ogni volta, se B 
 			 * sia sottotipo della classe considerata:
 			 * torna un RefTypeNode a tale classe qualora il controllo abbia, prima o poi, successo, null altrimenti*/
-			
+
 			RefTypeNode refB = (RefTypeNode) b; 
 			RefTypeNode refA = (RefTypeNode) a; 
 
@@ -114,12 +123,12 @@ public class FOOLlib {
 			return null;
 		}
 
-		//per a e b tipi funzionali con stesso numero di parametri
-		/*controlla se esiste lowest common ancestor dei tipi di ritorno di a e b (si chiama ricorsivamente) e se, 
+		/* Per A e B tipi funzionali con stesso numero di parametri
+		 * Controlla se esiste lowest common ancestor dei tipi di ritorno di A e B (si chiama ricorsivamente) e se, 
 		 * per ogni i, i tipi parametro i-esimi sono uno sottotipo dell'altro (metodo "isSubtype"):
-		 * â€¢ torna null se il controllo non ha successo; altrimenti
-		 * â€¢ torna un tipo funzionale che ha come tipo di ritorno il risultato della chiamata ricorsiva (covarianza) 
-		 * 	 e come tipo di parametro i-esimo il tipo che eÌ€ sottotipo dell'altro (controvarianza)*/
+		 * - torna null se il controllo non ha successo; altrimenti
+		 * - torna un tipo funzionale che ha come tipo di ritorno il risultato della chiamata ricorsiva (covarianza) 
+		 * 	 e come tipo di parametro i-esimo il tipo che e' sottotipo dell'altro (controvarianza)*/
 		if (a instanceof ArrowTypeNode && b instanceof ArrowTypeNode) { //Se sono tipi funzionali
 			ArrowTypeNode nodeB = (ArrowTypeNode) b; 
 			ArrowTypeNode nodeA = (ArrowTypeNode) a; 
@@ -146,7 +155,7 @@ public class FOOLlib {
 		}
 		return null;
 	}
-	
+
 	private static int labCount = 0; //nome etichette (univoche)
 	private static int funlabCount = 0; 
 	private static String funCode = ""; 
